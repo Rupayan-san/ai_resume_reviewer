c04c359ac807344055f08b67f5b7fd82124809b3
Rupayan <rikdasrik297@gmail.com>
2026-06-28 23:22:16 +0530

    Implement initial application structure with health check route and API response utility

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,30 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from "cors";
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('nice!');
-});
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+import heathCheckRouter from './routes/healthcheck.route.js'
+
+
+app.use('/api/healthCheck', heathCheckRouter)
+
+
+
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+  console.log(`app is runnning on port ${process.env.PORT}`);
 });
+
+
+
+export default app
